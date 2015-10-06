f6f9f96c2707232ac46c0e6abe23c67a9b8be39c
Hugues Pouillot <hugues.pouillot@happn.fr>
2015-10-06 14:38:02 +0200

    Use decorators existence in look command

--- pop/commands/look/look.py
+++ pop/commands/look/look.py
@@ -1,13 +1,14 @@
 import click
 from pop.main import pass_controller
-from pop.managers.manager_factory import ManagerFactory
 from pop.exceptions.no_project_error import NoProjectError
+from pop.utils.decorators import project_exists, data_exists
 
 @click.command('look', short_help='Look into data')
 @click.argument('table_name', required=False, type=str, default='default')
 @click.pass_obj
+@project_exists
+@data_exists
 def cli(ctl,table_name):
-	if not ctl.is_project_set:
-		raise NoProjectError()
 	data = ctl.get_table(table_name)
-	print data
+
+	click.echo(data.__str__())
